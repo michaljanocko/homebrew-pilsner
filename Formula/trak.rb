f8344c23a17f7ec275aae50d21169272fc680ad1
Michal Janočko <33790048+michaljanocko@users.noreply.github.com>
2021-03-16 18:27:29 -0500

    Update trak v1.1
    Trying to remove the checksum

--- Formula/trak.rb
+++ Formula/trak.rb
@@ -2,7 +2,6 @@ class Trak < Formula
   desc "Super-simple time tracker for your terminal"
   homepage "https://github.com/michaljanocko/trak"
   url "https://github.com/michaljanocko/trak/archive/v1.1.tar.gz"
-  sha256 "6e19421e3ae8232ea85255098b6acd53e4d293c7df2d10ecca175afbb83d795d"
   license "MIT"
 
   def install
